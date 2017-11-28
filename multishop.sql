626471c4e71b598954412784ee156211f5b3e40b
Pedro Villacreces <pedrovillacreces@gmail.com>
2017-11-28 23:35:50 +0100

    update database

--- multishop.sql
+++ multishop.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Server version:               10.1.26-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win32
--- HeidiSQL Version:             9.4.0.5188
+-- Versión del servidor:         10.1.28-MariaDB - mariadb.org binary distribution
+-- SO del servidor:              Win32
+-- HeidiSQL Versión:             9.4.0.5125
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -12,19 +12,19 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 
--- Dumping database structure for multishop
-CREATE DATABASE IF NOT EXISTS `multishop` /*!40100 DEFAULT CHARACTER SET latin1 COLLATE latin1_spanish_ci */;
+-- Volcando estructura de base de datos para multishop
+CREATE DATABASE IF NOT EXISTS `multishop` /*!40100 DEFAULT CHARACTER SET utf16 COLLATE utf16_spanish_ci */;
 USE `multishop`;
 
--- Dumping structure for table multishop.categories
+-- Volcando estructura para tabla multishop.categories
 CREATE TABLE IF NOT EXISTS `categories` (
   `id_category` int(2) NOT NULL AUTO_INCREMENT,
   `category` varchar(20) NOT NULL DEFAULT '',
   PRIMARY KEY (`id_category`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
--- Dumping structure for table multishop.codes
+-- La exportación de datos fue deseleccionada.
+-- Volcando estructura para tabla multishop.codes
 CREATE TABLE IF NOT EXISTS `codes` (
   `id_code` int(3) NOT NULL AUTO_INCREMENT,
   `code` varchar(10) NOT NULL DEFAULT '',
@@ -35,8 +35,8 @@ CREATE TABLE IF NOT EXISTS `codes` (
   CONSTRAINT `FK_id_customer_code` FOREIGN KEY (`id_customer`) REFERENCES `customers` (`id_customer`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
--- Dumping structure for table multishop.comments
+-- La exportación de datos fue deseleccionada.
+-- Volcando estructura para tabla multishop.comments
 CREATE TABLE IF NOT EXISTS `comments` (
   `id_comment` int(11) NOT NULL AUTO_INCREMENT,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -58,8 +58,8 @@ CREATE TABLE IF NOT EXISTS `comments` (
   CONSTRAINT `FK_id_product_comment` FOREIGN KEY (`id_product`) REFERENCES `products` (`id_product`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
--- Data exporting was unselected.
--- Dumping structure for table multishop.customers
+-- La exportación de datos fue deseleccionada.
+-- Volcando estructura para tabla multishop.customers
 CREATE TABLE IF NOT EXISTS `customers` (
   `id_customer` int(3) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL DEFAULT '',
@@ -74,8 +74,8 @@ CREATE TABLE IF NOT EXISTS `customers` (
   PRIMARY KEY (`id_customer`)
 ) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
--- Dumping structure for table multishop.deliveries
+-- La exportación de datos fue deseleccionada.
+-- Volcando estructura para tabla multishop.deliveries
 CREATE TABLE IF NOT EXISTS `deliveries` (
   `id_delivery` int(5) NOT NULL AUTO_INCREMENT,
   `delivery_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -95,8 +95,8 @@ CREATE TABLE IF NOT EXISTS `deliveries` (
   CONSTRAINT `FK_id_status` FOREIGN KEY (`id_status`) REFERENCES `delivery_status` (`id_delivery_status`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
--- Dumping structure for table multishop.delivery_details
+-- La exportación de datos fue deseleccionada.
+-- Volcando estructura para tabla multishop.delivery_details
 CREATE TABLE IF NOT EXISTS `delivery_details` (
   `id_product` int(11) NOT NULL,
   `quantity` int(1) NOT NULL DEFAULT '0',
@@ -107,24 +107,24 @@ CREATE TABLE IF NOT EXISTS `delivery_details` (
   CONSTRAINT `FKdelivery_product` FOREIGN KEY (`id_product`) REFERENCES `products` (`id_product`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
--- Dumping structure for table multishop.delivery_status
+-- La exportación de datos fue deseleccionada.
+-- Volcando estructura para tabla multishop.delivery_status
 CREATE TABLE IF NOT EXISTS `delivery_status` (
   `id_delivery_status` int(11) NOT NULL AUTO_INCREMENT,
   `status` varchar(50) COLLATE latin1_spanish_ci NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_delivery_status`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
--- Data exporting was unselected.
--- Dumping structure for table multishop.dispatch_status
+-- La exportación de datos fue deseleccionada.
+-- Volcando estructura para tabla multishop.dispatch_status
 CREATE TABLE IF NOT EXISTS `dispatch_status` (
   `id_dispatch_status` int(11) NOT NULL AUTO_INCREMENT,
   `status` varchar(50) COLLATE latin1_spanish_ci NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_dispatch_status`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
--- Data exporting was unselected.
--- Dumping structure for table multishop.images
+-- La exportación de datos fue deseleccionada.
+-- Volcando estructura para tabla multishop.images
 CREATE TABLE IF NOT EXISTS `images` (
   `id_image` int(4) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL DEFAULT '',
@@ -135,16 +135,16 @@ CREATE TABLE IF NOT EXISTS `images` (
   CONSTRAINT `FK_id_product_image` FOREIGN KEY (`id_product`) REFERENCES `products` (`id_product`)
 ) ENGINE=InnoDB AUTO_INCREMENT=102 DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
--- Dumping structure for table multishop.payments
+-- La exportación de datos fue deseleccionada.
+-- Volcando estructura para tabla multishop.payments
 CREATE TABLE IF NOT EXISTS `payments` (
   `id_payment` int(11) NOT NULL AUTO_INCREMENT,
   `type` varchar(50) COLLATE latin1_spanish_ci NOT NULL,
   PRIMARY KEY (`id_payment`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
--- Data exporting was unselected.
--- Dumping structure for table multishop.products
+-- La exportación de datos fue deseleccionada.
+-- Volcando estructura para tabla multishop.products
 CREATE TABLE IF NOT EXISTS `products` (
   `id_product` int(3) NOT NULL AUTO_INCREMENT,
   `product_name` varchar(250) NOT NULL DEFAULT '',
@@ -160,16 +160,16 @@ CREATE TABLE IF NOT EXISTS `products` (
   CONSTRAINT `FK_id_product_category` FOREIGN KEY (`id_category`) REFERENCES `categories` (`id_category`)
 ) ENGINE=InnoDB AUTO_INCREMENT=69 DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
--- Dumping structure for table multishop.roles
+-- La exportación de datos fue deseleccionada.
+-- Volcando estructura para tabla multishop.roles
 CREATE TABLE IF NOT EXISTS `roles` (
   `id_role` int(11) NOT NULL AUTO_INCREMENT,
   `role` varchar(50) COLLATE latin1_spanish_ci NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_role`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
--- Data exporting was unselected.
--- Dumping structure for table multishop.slider
+-- La exportación de datos fue deseleccionada.
+-- Volcando estructura para tabla multishop.slider
 CREATE TABLE IF NOT EXISTS `slider` (
   `id_slide` int(10) NOT NULL AUTO_INCREMENT,
   `url` varchar(200) COLLATE latin1_spanish_ci NOT NULL,
@@ -178,8 +178,8 @@ CREATE TABLE IF NOT EXISTS `slider` (
   PRIMARY KEY (`id_slide`)
 ) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
--- Data exporting was unselected.
--- Dumping structure for table multishop.stock_message
+-- La exportación de datos fue deseleccionada.
+-- Volcando estructura para tabla multishop.stock_message
 CREATE TABLE IF NOT EXISTS `stock_message` (
   `id_stock_message` int(5) NOT NULL AUTO_INCREMENT,
   `mail` varchar(100) CHARACTER SET latin1 NOT NULL DEFAULT '',
@@ -189,16 +189,16 @@ CREATE TABLE IF NOT EXISTS `stock_message` (
   CONSTRAINT `FK_id_stock_message_product` FOREIGN KEY (`id_product`) REFERENCES `products` (`id_product`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
--- Data exporting was unselected.
--- Dumping structure for table multishop.stock_switch
+-- La exportación de datos fue deseleccionada.
+-- Volcando estructura para tabla multishop.stock_switch
 CREATE TABLE IF NOT EXISTS `stock_switch` (
   `id` tinyint(1) NOT NULL AUTO_INCREMENT,
   `status` bit(1) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
--- Dumping structure for table multishop.subcategories
+-- La exportación de datos fue deseleccionada.
+-- Volcando estructura para tabla multishop.subcategories
 CREATE TABLE IF NOT EXISTS `subcategories` (
   `id_subcategory` int(11) NOT NULL AUTO_INCREMENT,
   `subcategory_name` varchar(50) COLLATE latin1_spanish_ci NOT NULL DEFAULT '',
@@ -208,31 +208,33 @@ CREATE TABLE IF NOT EXISTS `subcategories` (
   CONSTRAINT `FK_id_subcategory_category` FOREIGN KEY (`id_category`) REFERENCES `categories` (`id_category`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
--- Data exporting was unselected.
--- Dumping structure for table multishop.switch
+-- La exportación de datos fue deseleccionada.
+-- Volcando estructura para tabla multishop.switch
 CREATE TABLE IF NOT EXISTS `switch` (
   `id_switch` int(11) NOT NULL AUTO_INCREMENT,
   `status` bit(1) NOT NULL,
   PRIMARY KEY (`id_switch`)
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
--- Dumping structure for table multishop.users
+-- La exportación de datos fue deseleccionada.
+-- Volcando estructura para tabla multishop.users
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL DEFAULT '',
   `surname` varchar(50) DEFAULT NULL,
   `user_name` varchar(20) NOT NULL DEFAULT '',
   `email` varchar(50) NOT NULL DEFAULT '',
-  `rol` int(1) NOT NULL,
+  `id_role` int(1) NOT NULL,
   `photo` text,
   `password` varchar(50) NOT NULL DEFAULT '',
   `counter` int(3) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`),
+  KEY `FK_id_role` (`id_role`),
+  CONSTRAINT `FK_id_role` FOREIGN KEY (`id_role`) REFERENCES `roles` (`id_role`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
--- Dumping structure for table multishop.zip_files
+-- La exportación de datos fue deseleccionada.
+-- Volcando estructura para tabla multishop.zip_files
 CREATE TABLE IF NOT EXISTS `zip_files` (
   `id_files` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) CHARACTER SET latin1 NOT NULL DEFAULT '',
@@ -242,7 +244,7 @@ CREATE TABLE IF NOT EXISTS `zip_files` (
   CONSTRAINT `FK_id_product_files` FOREIGN KEY (`id_product`) REFERENCES `products` (`id_product`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
--- Data exporting was unselected.
+-- La exportación de datos fue deseleccionada.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
